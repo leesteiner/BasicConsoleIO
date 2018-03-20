04733ea70d958b0b59f52fce351b7a18324d9ba2
Lee Steiner <leealansteiner@gmail.com>
2018-03-20 15:08:15 -0400

    Initial Commit to Test Branch

--- BasicConsoleIO/Program.cs
+++ BasicConsoleIO/Program.cs
@@ -12,6 +12,7 @@ namespace BasicConsoleIO
         {
             Console.WriteLine("***** Basic Console I/O *****");
             GetUserData();
+            FormatNumericalData();
             Console.ReadLine();
         }
 
@@ -32,6 +33,22 @@ namespace BasicConsoleIO
 
             //Restore previous color.
             Console.ForegroundColor = prevColor;
+        }
+
+        private static void FormatNumericalData()
+        {
+            Console.WriteLine("The value 99999 in various formats:");
+            Console.WriteLine("c format: {0:c}", 9999);
+            Console.WriteLine("d9 format: {0:d9}", 9999);
+            Console.WriteLine("f3 format: {0:f3}", 9999);
+            Console.WriteLine("n format: {0:n}", 9999);
+
+            //Notice that upper- or lowercasing for hex
+            //determines if letters are upper- or lowercase.
+            Console.WriteLine("E format: {0:E}", 9999);
+            Console.WriteLine("e format: {0:e}", 9999);
+            Console.WriteLine("X format: {0:X}", 9999);
+            Console.WriteLine("x format: {0:x}", 9999);
         }
     }
 }
